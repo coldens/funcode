cdb0d3eba2da0e71180e3ec2b2e93a428b5730f7
Jose Gonzalez <jdgg2414@gmail.com>
2023-09-02 10:19:08 -0500

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 export { createPipe } from './pipe/createPipe';
 export { pipe } from './pipe/pipe';
-export { eq } from './when/eq/eq';
-export { gt } from './when/gt/gt';
-export { gte } from './when/gte/gte';
-export { lt } from './when/lt/lt';
-export { lte } from './when/lte/lte';
 export { tap } from './tap/tap';
+export * from './when';
---
+++ src/when/index.ts
@@ -0,0 +1,5 @@
+export { eq } from './eq/eq';
+export { gt } from './gt/gt';
+export { gte } from './gte/gte';
+export { lt } from './lt/lt';
+export { lte } from './lte/lte';
